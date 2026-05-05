99206f1eeb85bbd0fe6bd1cc6eacdd0bbd6432f2
chaewon41 <chaew0m3x@gmail.com>
2026-05-05 15:28:59 +0900

    chore: theme 설정 추가 (#11)

--- chaewon/week3/everytime-component/src/App.jsx
+++ chaewon/week3/everytime-component/src/App.jsx
@@ -1,19 +1,20 @@
 import styled from '@emotion/styled'
 import GlobalStyles from './styles/GlobalStyle'
+import { ThemeProvider } from '@emotion/react'
+import { theme } from './styles/theme'
 
 export default function App() {
 
   return (
-    <>
-    <GlobalStyles />
-    <Title>초기세팅 확인~~</Title>
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Title>초기세팅 확인~~</Title>
+    </ThemeProvider>
   )
 
 }
 
 const Title = styled.h1 `
-  font-size: 100px;
-  color: red;
+  ${({theme}) => theme.typography.title03}
 `
 
---
+++ chaewon/week3/everytime-component/src/styles/theme.js
@@ -0,0 +1,50 @@
+export const theme = {
+    colors: {
+        primary: {
+            red: '#F91F15',
+        },
+
+        gary: {
+            '500': '#A6A6A6',
+            '700': '#444',
+        },
+    },
+
+    typography: {
+        title03: {
+            fontSize: '16px',
+            fontWeight: 700,
+            lineHeight: '130%',
+            letterSpacing: '0.16px',
+        },
+
+        title08: {
+            fontSize: '16px',
+            fontWeight: 500,
+            lineHeight: '130%',
+            letterSpacing: '-0.16px',
+        },
+
+        body05: {
+            fontSize: '15px',
+            fontWeight: 500,
+            lineHeight: '130%',
+            letterSpacing: '-0.3px',
+        },
+
+        body07: {
+            fontSize: '12px',
+            fontWeight: 500,
+            lineHeight: '130%',
+            letterSpacing: '-0.12px',
+        },
+
+        caption06: {
+            fontSize: '11px',
+            fontWeight: 500,
+            lineHeight: '130%',
+        }
+
+    }
+
+}
