24aa20350a933a5d2302df7052c98c863783cdd5
yumin-kim4757 <rladbals0211@gmail.com>
2026-05-05 23:06:43 +0900

    feat: 장터 게시판 데이터 생성

--- yumin/week3/src/components/Board/BoardIData.js
+++ yumin/week3/src/components/Board/BoardIData.js
@@ -21,3 +21,26 @@ const freeBoardItems = [
   },
 ];
 export default freeBoardItems;
+
+const marketBoardItems = [
+  {
+    id: 1,
+    text: "햄버거 쿠폰 반값에 팝니다",
+    createdAt: "2026-04-27T10:10:00",
+  },
+  {
+    id: 2,
+    text: "운영체제 책 싸게 팝니다",
+    createdAt: "2026-04-30T14:55:57",
+  },
+  {
+    id: 3,
+    text: "원룸 양도 받을 분 있나요",
+    createdAt: "2026-5-01T17:27:00",
+  },
+  {
+    id: 4,
+    text: "데이터베이스 기출 팝니다",
+    createdAt: "2026-05-05T23:05:00",
+  },
+];
