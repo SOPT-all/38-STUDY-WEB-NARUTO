39d56ecfd7a734eaec7c6b64f26264d75f716d66
chaewon41 <chaew0m3x@gmail.com>
2026-05-05 14:47:59 +0900

    chore: GlobalStyle 및 폰트 설정 추가 (#11)

--- chaewon/week3/everytime-component/src/App.jsx
+++ chaewon/week3/everytime-component/src/App.jsx
@@ -1,10 +1,13 @@
 import styled from '@emotion/styled'
+import GlobalStyles from './styles/GlobalStyle'
 
 export default function App() {
 
   return (
+    <>
+    <GlobalStyles />
     <Title>초기세팅 확인~~</Title>
-
+    </>
   )
 
 }
