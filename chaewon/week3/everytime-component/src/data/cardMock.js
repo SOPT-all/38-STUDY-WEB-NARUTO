fe58e3eec2ff98c9930e9a34c122edcb65d9e24e
chaewon41 <chaew0m3x@gmail.com>
2026-05-05 17:55:00 +0900

    chore: 카드 리스트 목데이터 추가 (#11)

---
+++ chaewon/week3/everytime-component/src/data/cardMock.js
@@ -0,0 +1,22 @@
+export const cardMock = [
+    {
+        id: 1,
+        image: `/images/product-1.png`,
+        title: "어서와 파이썬은 처음이지!",
+        price: 35000,
+    },
+        
+    {
+        id: 2,
+        image: `/images/product-2.jpg`,
+        title: "나루토 화이팅!!!",
+        price: 42000,
+    },
+
+    {
+        id: 3,
+        image: `/images/product-3.jpg`,
+        title: "웹 개발 쉽지 않다..정말로.....",
+        price: 12000,
+    },
+]
