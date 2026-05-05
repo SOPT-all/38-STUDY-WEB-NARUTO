07ff5ceca6cd9cfd589960cc42366e26ea01cfd9
chaewon41 <chaew0m3x@gmail.com>
2026-05-05 18:17:17 +0900

    refactor: 게시판 유형 구분을 위한 목데이터 구조 변경 (#11)

--- chaewon/week3/everytime-component/src/data/listMock.js
+++ chaewon/week3/everytime-component/src/data/listMock.js
@@ -2,31 +2,71 @@ export const listMock = [
     {
         id: 1,
         title: "나루토 화이팅",
+        category: "question",
         createdAt: "2026-04-29T15:10:00",
     },
 
     {
         id: 2,
         title: "LG 야구 보다가 또 분노",
+        category: "question",
         createdAt: "2026-05-01T17:10:56",
     },
 
     {
         id: 3,
         title: "컴포넌트 만들기 과제~",
+        category: "question",
         createdAt: "2026-03-01T11:10:56",
     },
 
     {
         id: 4,
         title: "이건 진짜 너무 길게 써서 UI가 어떻게 깨지는지 테스트해보는 제목입니다. 말줄임이 제대로 되는지 확인해야 되니까.. 근데 할 말이 없어서 길게 쓰기 힘들다",
+        category: "question",
         createdAt: "2025-02-06T09:10:56",
     },
 
     {
         id: 5,
         title: "메가커피 초코베리믹스 먹고싶다",
+        category: "question",
         createdAt: "2026-05-05T16:10:11",
+    },
+
+    {
+        id: 6,
+        title: "토익 스피킹 책 팔아요",
+        category: "market",
+        createdAt: "2026-05-01T19:10:11",
+    },
+
+    {
+        id: 7,
+        title: "장터 게시판 데이터~~",
+        category: "market",
+        createdAt: "2026-05-01T23:10:11",
+    },
+
+    {
+        id: 8,
+        title: "과제를 언제 다 할 수 있을까요..",
+        category: "market",
+        createdAt: "2026-05-05T18:00:11",
+    },
+
+    {
+        id: 9,
+        title: "이것도 UI 테스트용으로 텍스트 길게 적고 말줄임표 잘 적용되는지 볼게요",
+        category: "market",
+        createdAt: "2026-05-01T16:10:11",
+    },
+
+    {
+        id: 10,
+        title: "오늘 저녁 회 먹는다 와!!!",
+        category: "market",
+        createdAt: "2026-05-05T18:10:11",
     },
 
 ]
