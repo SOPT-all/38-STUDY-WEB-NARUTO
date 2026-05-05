085cf5f34c400deb2d1e05af66d207a5c6551f6e
chaewon41 <chaew0m3x@gmail.com>
2026-05-05 18:01:08 +0900

    chore: App.jsx 컴포넌트 연결 (#11)

--- chaewon/week3/everytime-component/src/App.jsx
+++ chaewon/week3/everytime-component/src/App.jsx
@@ -1,20 +1,22 @@
-import styled from '@emotion/styled'
+// import styled from '@emotion/styled'
 import GlobalStyles from './styles/GlobalStyle'
 import { ThemeProvider } from '@emotion/react'
 import { theme } from './styles/theme'
+import BoardList from './components/BoardList'
+import CardList from './components/CardList'
+import { listMock } from './data/listMock'
+import { cardMock } from './data/cardMock'
 
 export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Title>초기세팅 확인~~</Title>
+      <BoardList boardTitle="자유 게시판" lists={listMock} />
+      <CardList lists={cardMock}></CardList>
     </ThemeProvider>
   )
 
 }
 
-const Title = styled.h1 `
-  ${({theme}) => theme.typography.title03}
-`
 
