45d44c8a104e268338e2d066a94784e3114b2c91
yumin-kim4757 <rladbals0211@gmail.com>
2026-05-05 21:05:13 +0900

    feat: 자유 게시판 데이터 파일 생성 (#6)

--- yumin/week3/src/components/BoardBox.jsx
+++ yumin/week3/src/components/BoardBox.jsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import BoardHeader from "./BoardHeader";
 import BoardItems from "./BoardItems";
+import { freeBoardItems } from "./BoardIData";
 
 export const Box = styled.main`
   display: flex;
@@ -16,10 +17,6 @@ function BoardBox(props) {
   return (
     <Box>
       <BoardHeader title={props.title} />
-      <BoardItems text="수험생인데" createdAt="12분 전" />
-      <BoardItems text="한국역사의 이해 한준수교수님" createdAt="42분 전" />
-      <BoardItems text="아니 뭔 시험을 이렇게 내냐" createdAt="04/29 08:54" />
-      <BoardItems text="나비야나비야어디를가느냐" createdAt="04/29 03:54" />
     </Box>
   );
 }
---
+++ yumin/week3/src/components/BoardIData.js
@@ -0,0 +1,19 @@
+const freeBoardItems = [
+  {
+    text: "수험생인데",
+    createdAt: "12분 전",
+  },
+  {
+    text: "한국역사의 이해 한준수교수님",
+    createdAt: "42분 전",
+  },
+  {
+    text: "아니 뭔 시험을 이렇게 내냐",
+    createdAt: "04/29 08:54",
+  },
+  {
+    text: "나비야나비야어디를가느냐",
+    createdAt: "04/29 03:54",
+  },
+];
+export default freeBoardItems;
