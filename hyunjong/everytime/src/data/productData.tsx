b722add37daea6edc12c5673fdbcbbf463dcc406
Navi <masimello0001@gmail.com>
2026-05-05 14:13:34 +0900

    chore: product 이미지 추가 (#9)

---
+++ hyunjong/everytime/src/data/productData.tsx
@@ -0,0 +1,10 @@
+// 상품 데이터
+import product1 from "../assets/product1.jpg";
+import product2 from "../assets/product2.jpg";
+import product3 from "../assets/product3.jpg";
+
+export const products = [
+  { id: 1, title: "웹 개발 쉽지 않다", price: 1004, image: product1 },
+  { id: 2, title: "나루토귀엽", price: 210000, image: product2 },
+  { id: 3, title: "어서와 파이썬은 처음이지!", price: 8000, image: product3 },
+];
