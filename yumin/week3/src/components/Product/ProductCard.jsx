591a5152f38ed70d39ac1ef7a098c8e71550e5d0
yumin-kim4757 <rladbals0211@gmail.com>
2026-05-06 06:01:53 +0900

    design: 상품 카드 컴포넌트 세부 UI 구현 #17

--- yumin/week3/src/components/Product/ProductCard.jsx
+++ yumin/week3/src/components/Product/ProductCard.jsx
@@ -2,23 +2,35 @@ import styled from "@emotion/styled";
 
 export const Card = styled.div`
   display: flex;
+  flex: 1;
   flex-direction: column;
-  margin-right: 2px;
+  border: 1px solid #9f9494;
+  gap: 2px;
 `;
 
 export const Image = styled.img`
   width: 100%;
-  height: 100%;
-  object-fit: contain;
+  height: 170px;
+  object-fit: cover;
 `;
 
 export const Title = styled.h1`
+  margin: 0;
+  height: 40px;
+  display: fles;
+  align-items: center;
   font-size: 15px;
+  text-align: left;
+  padding-left: 10px;
 `;
 
 export const Price = styled.div`
-  font-size: 25px;
+  font-size: 13px;
   color: #d51717;
+  text-align: left;
+  margin-top: auto;
+  padding-left: 10px;
+  padding-bottom: 7px;
 `;
 
 function ProductCard(props) {
